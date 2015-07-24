b84e1adebc56a020ead2c56c06c14ed61bf71bc1
Finnur Sigurðsson <finnursigu@gmail.com>
2015-07-24 17:07:40 +0000

    cleanup

--- public/assets/main.bundle.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([0,1],[function(module,exports){eval("//require('../less/main.less');\n\nconsole.log('Hello world!!');\n\n/*****************\n ** WEBPACK FOOTER\n ** ./scripts/main.js\n ** module id = 0\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./scripts/main.js?")}]);
